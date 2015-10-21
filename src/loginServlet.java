884c1d5ea2bdd1407d62a1cd0a556e7dd19000e8
Filehp <filipthebob@web.de>
2015-10-21 12:00:30 +0200

    Minimalbeispiel funzt, Verbindung funzt

--- src/loginServlet.java
+++ src/loginServlet.java
@@ -2,6 +2,13 @@
  * Created by Malte on 21.10.2015.
  */
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 @WebServlet("/loginServlet")
 public class loginServlet extends HttpServlet {
 
@@ -11,12 +18,12 @@ public class loginServlet extends HttpServlet {
         // code to process the form...
         String user = request.getParameter("username");
         String pwd = request.getParameter("password");
-        DatabaseHelper db=new DatabaseHelper();
-        if(db.login()==true){
+        DatabaseHelper db = new DatabaseHelper();
+        if (db.login(user, pwd) == true) {
             response.sendRedirect("index.html");
-        }else{
+        } else {
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/test.html");
-            PrintWriter out= response.getWriter();
+            PrintWriter out = response.getWriter();
             out.println("<font color=red>Either user name or password is wrong.</font>");
             rd.include(request, response);
         }
