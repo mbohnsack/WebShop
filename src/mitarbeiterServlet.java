b743dfb4a9c50fd897babb58ec73fbfbf5b8de7f
wesley.rubio.cueva <wesley_rubio@hotmail.com>
2015-10-22 13:24:59 +0200

    Neu: -Mitarbeiter_erstellen Servlet (zugehörige html seite sollte erstellt werden) -Databasehelper überarbeitet und Passwörter werden mit MD5 verschlüsselt

---
+++ src/mitarbeiterServlet.java
@@ -0,0 +1,36 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by wesley.rubio.cueva on 22.10.2015.
+ */
+
+@WebServlet("/mitarbeiterServlet")
+public class mitarbeiterServlet extends HttpServlet{
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+        DatabaseHelper db = new DatabaseHelper();
+        String name = request.getParameter("name");
+        String passwort = request.getParameter("passwort");
+
+        if(db.mitarbeiterFrei(name)){
+            db.createMitarbeiter(name, passwort);
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/mitarbeitererstellen.html"); //Das html-Dokument muss noch erstellt werden.
+            PrintWriter out= response.getWriter();
+            out.println("<font color=green>The process was succesfull.</font>");
+            rd.include(request, response);        }
+        else{
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/mitarbeitererstellen.html");
+            PrintWriter out= response.getWriter();
+            out.println("<font color=red>The name does already exist.</font>");
+            rd.include(request, response);
+
+        }
+    }
+}
