95cb15dbd93ae708f6c3732caad5c70198c2c8ff
mbohnsack <icqtest6@web.de>
2015-10-26 15:35:31 +0100

    Test Mail XI

--- src/SendMailSSL.java
+++ src/SendMailSSL.java
@@ -12,17 +12,20 @@ public class SendMailSSL {
         final String password = "YXCVBNM;";
 
         Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
 
-        Session session = Session.getInstance(props,
+        Session session = Session.getDefaultInstance(props,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
+                        return new PasswordAuthentication("username", "password");
                     }
                 });
+
 
         try {
 
@@ -42,3 +45,4 @@ public class SendMailSSL {
         }
     }
 }
+
