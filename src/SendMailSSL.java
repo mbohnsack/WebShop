c91a0157f31ff222262f824574c770fdf4336914
mbohnsack <icqtest6@web.de>
2015-10-26 14:14:06 +0100

    Test Mail

---
+++ src/SendMailSSL.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Malte on 26.10.2015.
+ */
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+public class SendMailSSL {
+}
