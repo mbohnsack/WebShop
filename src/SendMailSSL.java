41020834085928df3eadd238974462e6571b72b0
mbohnsack <icqtest6@web.de>
2015-10-26 14:18:44 +0100

    Test Mail II

--- src/SendMailSSL.java
+++ src/SendMailSSL.java
@@ -10,4 +10,39 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 public class SendMailSSL {
+    public static void sendMail() {
+
+        final String username = "hipsterrentalcorp@gmail.com";
+        final String password = "YXCVBNM;";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("hipsterrentalcorp@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("m.bohnsack99@web.de"));
+            message.setSubject("Testing Subject");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n No spam to my email, please!");
+
+            Transport.send(message);
+
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
