a221e5e96704fbc367043369233d409bb02e983f
wesley.rubio.cueva <wesley_rubio@hotmail.com>
2015-10-26 16:26:30 +0100

    Mail

--- src/SendMailSSL.java
+++ src/SendMailSSL.java
@@ -13,13 +13,18 @@ public class SendMailSSL {
 
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.starttls.enable", true);
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.port", "587");
 
-        Session session = Session.getDefaultInstance(props);
+        Authenticator auth = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username,password);
+            }
+        };
+
+        Session session = Session.getDefaultInstance(props,auth);
 
 
         try {
