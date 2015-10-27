f9147d0329ba1766eb8992a86917b03d1d653fc2
BrotxD <brot-online@gmx.de>
2015-10-27 09:36:29 +0100

    LoginServlet erweitert

--- src/loginServlet.java
+++ src/loginServlet.java
@@ -23,7 +23,7 @@ public class loginServlet extends HttpServlet {
 
         Boolean login = false;
         DatabaseHelper db = new DatabaseHelper();
-        if(targetPage.equals("main.jsp")){
+        if(targetPage.equals("general/setCookie.js")){
             login = db.loginMitarbeiter(user, pwd);
         } else if(targetPage.equals("")){
             login = db.loginKunde(user, pwd);
