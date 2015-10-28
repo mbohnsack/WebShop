eb6b33b0cf80560cf33b8352ae38f1697cc6a98d
mbohnsack <icqtest6@web.de>
2015-10-28 11:34:11 +0100

    Neue Struktur

--- src/loginServlet.java
+++ src/loginServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("loginServlet")
+@WebServlet("/loginServlet")
 public class loginServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request,
