2c4fc91fff88718784109874163f1d0fc37e2124
Matthias Findeisen <m.findeisen@solprime.de>
2015-11-05 15:41:42 +0100

    KD logout

--- src/deleteKundeServlet.java
+++ src/deleteKundeServlet.java
@@ -1,24 +1,42 @@
 import project.DatabaseHelper;
+import project.loginCookie;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/deleteKundeServlet")
 public class deleteKundeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response) throws ServletException, IOException {
-        String kundeUsername = request.getParameter("deleteKunde");
 
         DatabaseHelper db = new DatabaseHelper();
-        db.deleteKunde(kundeUsername);
+        HttpSession session = request.getSession();
 
+        String user = "";
+        loginCookie loginDaten = (loginCookie)
+                session.getAttribute("loginCookie");
+        if (loginDaten != null) {
+            if (loginDaten.getRolle() == "Kunde") {
+                user = loginDaten.getUsername();
+            }
+        }
 
-        String url = "general/logout.jsp";
-        response.sendRedirect( url );
+        db.deleteKunde(user); //KD löschen
+        session.removeAttribute("loginCookie"); //cookie attribut entfernen
+
         db.disconnectDatabase();
+
+        String url = "index.jsp";
+        response.sendRedirect( url );
+
+
+
+
+
     }
 }
