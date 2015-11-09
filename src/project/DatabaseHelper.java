1d3e4d52725943149d66346f988ee5e175a66214
wesley.rubio.cueva <wesley_rubio@hotmail.com>
2015-11-09 18:55:21 +0100

    deleteProduct fix

--- src/project/DatabaseHelper.java
+++ src/project/DatabaseHelper.java
@@ -398,6 +398,7 @@ public class DatabaseHelper{
     public void deleteProduct(Integer id){
         try {
             stmt.executeUpdate("DELETE FROM tbl_bild WHERE prod_id ="+ id);
+            stmt.executeUpdate("DELETE FROM tbl_lagerliste WHERE prod_id ="+ id);
             stmt.executeUpdate("DELETE FROM tbl_produkt WHERE prod_id = " + id);
         } catch (SQLException e) {
             e.printStackTrace();
